db04fa9a155489af15b6ef6e678e3c65f987e7de
jsanjak <jsanjak@gmail.com>
2023-02-22 09:52:58 -0500

    ready for hpc

--- PyMC_model_script_jss.py
+++ PyMC_model_script_jss.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import sys
 import multiprocessing as mp
-mp.set_start_method('forkserver', force=True)
 
 import pymc as pm
 import pandas as pd
@@ -16,153 +16,163 @@ from sklearn.compose import ColumnTransformer
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler,LabelBinarizer
 import pickle
-# from tqdm import tqdm
 
-# ### Stack 50 assays together along with assay meta info
+#TODO:
+# convert the sections below into readable functions
+# increase the tune and draw values in the model
+# add the prediction components back in
 
-xls = pd.ExcelFile('Tox21/assay_list.xls')
+def main() -> int:
+    # ### Stack 50 assays together along with assay meta info
+    mp.set_start_method('forkserver', force=True)
 
-# to read just one sheet to dataframe:
-df = []
-for i in range(len(xls.sheet_names)):
-    df1 = xls.parse(i)
-    df.append(df1)
+    xls = pd.ExcelFile('Tox21/assay_list.xls')
 
-xls2 = pd.ExcelFile('Tox21/assay_list2.xls')
+    # to read just one sheet to dataframe:
+    df = []
+    for i in range(len(xls.sheet_names)):
+        df1 = xls.parse(i)
+        df.append(df1)
 
-for i in range(len(xls2.sheet_names)):
-    df1 = xls2.parse(i)
-    df.append(df1)
+    xls2 = pd.ExcelFile('Tox21/assay_list2.xls')
 
-t = pd.DataFrame({'Cell_Line': ['DT40','DT40', 'DT40'], 'ProtocolName':['tox21-dt40-p1_100', 'tox21-dt40-p1_653', 'tox21-dt40-p1_657']})
+    for i in range(len(xls2.sheet_names)):
+        df1 = xls2.parse(i)
+        df.append(df1)
 
-assay_info_tox21 = pd.read_csv('Tox21/assay_meta_data.csv')
-assay_info_tox21 = assay_info_tox21.drop(assay_info_tox21.columns[0], axis = 1)
-assay_info_tox21['Cell_Line'] = assay_info_tox21['Cell_Line'].str.replace(r'*', '', regex=True)
-assay_info_tox21 = assay_info_tox21[['Cell_Line', 'ProtocolName']]
-assay_info_tox21 = assay_info_tox21.drop([49])
-assay_info_tox21 = pd.concat([assay_info_tox21, t]).reset_index(drop=True)
+    t = pd.DataFrame({'Cell_Line': ['DT40','DT40', 'DT40'], 'ProtocolName':['tox21-dt40-p1_100', 'tox21-dt40-p1_653', 'tox21-dt40-p1_657']})
 
-assay_info = pd.read_excel('Tox21/SampleMeta_Data_update.xlsx')
-assay_info = assay_info.drop(assay_info.columns[6:], axis=1).iloc[:13,:].rename(columns = {assay_info.columns[5]: 'Tissue_Type2'})
-assay_info = assay_info.rename(columns = {assay_info.columns[0]: 'Cell_Line'})
-assay_info['Cell_Line'] = assay_info['Cell_Line'].str.replace(r'*', '', regex=True).str.replace(' ', '')
-assay_info['Cell_Type'] = assay_info['Cell_Type'].str.replace(r'*', '', regex=True)
-assay_info = assay_info.join(assay_info_tox21.set_index('Cell_Line'), on='Cell_Line')
+    assay_info_tox21 = pd.read_csv('Tox21/assay_meta_data.csv')
+    assay_info_tox21 = assay_info_tox21.drop(assay_info_tox21.columns[0], axis = 1)
+    assay_info_tox21['Cell_Line'] = assay_info_tox21['Cell_Line'].str.replace(r'*', '', regex=True)
+    assay_info_tox21 = assay_info_tox21[['Cell_Line', 'ProtocolName']]
+    assay_info_tox21 = assay_info_tox21.drop([49])
+    assay_info_tox21 = pd.concat([assay_info_tox21, t]).reset_index(drop=True)
 
-df_list = []
-for i in range(len(df)):
-    one_assay = df[i]
-    one_assay = one_assay.drop(one_assay.columns[0], axis=1).drop_duplicates()
-    one_assay['ProtocolName'] = one_assay.columns[0]
-    one_assay = one_assay.rename(columns = {one_assay.columns[0]: 'Outcome'})
-    df_list.append(one_assay)
+    assay_info = pd.read_excel('Tox21/SampleMeta_Data_update.xlsx')
+    assay_info = assay_info.drop(assay_info.columns[6:], axis=1).iloc[:13,:].rename(columns = {assay_info.columns[5]: 'Tissue_Type2'})
+    assay_info = assay_info.rename(columns = {assay_info.columns[0]: 'Cell_Line'})
+    assay_info['Cell_Line'] = assay_info['Cell_Line'].str.replace(r'*', '', regex=True).str.replace(' ', '')
+    assay_info['Cell_Type'] = assay_info['Cell_Type'].str.replace(r'*', '', regex=True)
+    assay_info = assay_info.join(assay_info_tox21.set_index('Cell_Line'), on='Cell_Line')
 
-df_stack_50 = pd.concat(df_list)
-df_stack_50 = df_stack_50.join(assay_info.set_index('ProtocolName'), on='ProtocolName')
-# ### Train/Test Split & Bayesian model coordination
+    df_list = []
+    for i in range(len(df)):
+        one_assay = df[i]
+        one_assay = one_assay.drop(one_assay.columns[0], axis=1).drop_duplicates()
+        one_assay['ProtocolName'] = one_assay.columns[0]
+        one_assay = one_assay.rename(columns = {one_assay.columns[0]: 'Outcome'})
+        df_list.append(one_assay)
 
-df_stack_50.Outcome = df_stack_50.Outcome.astype(object)
+    df_stack_50 = pd.concat(df_list)
+    df_stack_50 = df_stack_50.join(assay_info.set_index('ProtocolName'), on='ProtocolName')
+    # ### Train/Test Split & Bayesian model coordination
 
-# Scale numeric chemical descriptors
-numeric_columns=list(df_stack_50.select_dtypes(['float64', 'int64']).columns)
-categorical_columns=list(df_stack_50.select_dtypes('object').columns)
+    df_stack_50.Outcome = df_stack_50.Outcome.astype(object)
 
-pipeline=ColumnTransformer([
-    ('num',StandardScaler(),numeric_columns),
-    ('cat', 'passthrough', categorical_columns)])
+    # Scale numeric chemical descriptors
+    numeric_columns=list(df_stack_50.select_dtypes(['float64', 'int64']).columns)
+    categorical_columns=list(df_stack_50.select_dtypes('object').columns)
 
-chem_des_scale = pipeline.fit_transform(df_stack_50)
-chem_des_scale = pd.DataFrame(chem_des_scale)
+    pipeline=ColumnTransformer([
+        ('num',StandardScaler(),numeric_columns),
+        ('cat', 'passthrough', categorical_columns)])
 
-col_name = list(df_stack_50.columns)
-col_name.pop(0)
-col_name.pop(0)
-col_name.insert(204, 'Outcome')
-col_name.insert(205,'SMILES')
-chem_des_scale.columns = col_name
+    chem_des_scale = pipeline.fit_transform(df_stack_50)
+    chem_des_scale = pd.DataFrame(chem_des_scale)
 
-df_try = chem_des_scale.iloc[:100, :]
-factorized_protocols = pd.factorize(chem_des_scale.ProtocolName)
+    col_name = list(df_stack_50.columns)
+    col_name.pop(0)
+    col_name.pop(0)
+    col_name.insert(204, 'Outcome')
+    col_name.insert(205,'SMILES')
+    chem_des_scale.columns = col_name
 
-X = chem_des_scale.iloc[:, :204]
-X = chem_des_scale.iloc[:, :20]
-y = chem_des_scale['Outcome']
-assay_info_try = chem_des_scale.iloc[:,206:]
+    df_try = chem_des_scale.iloc[:100, :]
+    factorized_protocols = pd.factorize(chem_des_scale.ProtocolName)
 
-X_train, X_test, y_train, y_test, assay_info_train, assay_info_test = train_test_split(X, y, assay_info_try, test_size=0.2, random_state=42)
-X4_bayes = X_train
+    X = chem_des_scale.iloc[:, :204]
+    X = chem_des_scale.iloc[:, :20]
+    y = chem_des_scale['Outcome']
+    assay_info_try = chem_des_scale.iloc[:,206:]
 
-X4_bayes.insert(0,'Intercept',1)
-X4_bayes = np.asarray(X4_bayes, dtype="float64")
+    X_train, X_test, y_train, y_test, assay_info_train, assay_info_test = train_test_split(X, y, assay_info_try, test_size=0.2, random_state=42)
+    X4_bayes = X_train
 
-factorized_protocols = pd.factorize(assay_info_train.ProtocolName)
+    X4_bayes.insert(0,'Intercept',1)
+    X4_bayes = np.asarray(X4_bayes, dtype="float64")
 
-Y4_bayes = pd.factorize(y_train)
+    factorized_protocols = pd.factorize(assay_info_train.ProtocolName)
 
-coords_simulated = {
-    'obs_id': np.arange(X4_bayes.shape[0]),
-    'chem_descrip': np.arange(X4_bayes.shape[1]),
-    'protocol':list(factorized_protocols[1]),
-    'params':['beta_{0}'.format(i) for i in range(X4_bayes.shape[1])]
-}
+    Y4_bayes = pd.factorize(y_train)
 
-organisms = pd.factorize(assay_info_train.drop_duplicates().Organism)
-tissues_4 = pd.factorize(assay_info_train.drop_duplicates().Tissue_Type4)
-tissues_2 = pd.factorize(assay_info_train.drop_duplicates().Tissue_Type2)
-gender = pd.factorize(assay_info_train.drop_duplicates().Gender)
-cell_type = pd.factorize(assay_info_train.drop_duplicates().Cell_Type)
+    coords_simulated = {
+        'obs_id': np.arange(X4_bayes.shape[0]),
+        'chem_descrip': np.arange(X4_bayes.shape[1]),
+        'protocol':list(factorized_protocols[1]),
+        'params':['beta_{0}'.format(i) for i in range(X4_bayes.shape[1])]
+    }
 
-lambdas = pd.read_csv('Tox21/log_reg_w_lambda_sig.csv')['0'].tolist()
+    organisms = pd.factorize(assay_info_train.drop_duplicates().Organism)
+    tissues_4 = pd.factorize(assay_info_train.drop_duplicates().Tissue_Type4)
+    tissues_2 = pd.factorize(assay_info_train.drop_duplicates().Tissue_Type2)
+    gender = pd.factorize(assay_info_train.drop_duplicates().Gender)
+    cell_type = pd.factorize(assay_info_train.drop_duplicates().Cell_Type)
 
-print('start model setup')
-with pm.Model(coords=coords_simulated) as assay_level_model:
-    x = pm.Data('x', X4_bayes, mutable = True)
-    protocol_idx = pm.Data("protocol_idx", list(factorized_protocols[0]), mutable=True)
-    organism_idx = pm.Data("organism_idx", organisms[0], mutable=True)
-    tissue_2_idx = pm.Data("tissue_2_idx", tissues_2[0], mutable=True)
-    tissue_4_idx = pm.Data("tissue_4_idx", tissues_4[0], mutable=True)
-    gender_idx = pm.Data("gender_idx", gender[0], mutable=True)
-    cell_type_idx = pm.Data("cell_type_idx", cell_type[0], mutable=True)
+    lambdas = pd.read_csv('Tox21/log_reg_w_lambda_sig.csv')['0'].tolist()
 
-    # prior stddev in intercepts & slopes (variation across protocol):
-    sd_dist = pm.Exponential.dist(1.0)
-    
-    # get back standard deviations and rho:
-    chol, corr, stds = pm.LKJCholeskyCov("chol", n=X4_bayes.shape[1], eta=2.0, sd_dist=sd_dist, compute_corr=True)
-    
-    #hyperpriors and priors for average betas:
-    beta_list = []
-    for i in range(X4_bayes.shape[1]):
-        gbeta = pm.Normal("g_beta_{0}".format(i), mu=0.0, sigma=10.0, shape=6)
+    print('start model setup')
+    with pm.Model(coords=coords_simulated) as assay_level_model:
+        x = pm.Data('x', X4_bayes, mutable = True)
+        protocol_idx = pm.Data("protocol_idx", list(factorized_protocols[0]), mutable=True)
+        organism_idx = pm.Data("organism_idx", organisms[0], mutable=True)
+        tissue_2_idx = pm.Data("tissue_2_idx", tissues_2[0], mutable=True)
+        tissue_4_idx = pm.Data("tissue_4_idx", tissues_4[0], mutable=True)
+        gender_idx = pm.Data("gender_idx", gender[0], mutable=True)
+        cell_type_idx = pm.Data("cell_type_idx", cell_type[0], mutable=True)
 
-        mu_gbeta = gbeta[0] + gbeta[1] * organism_idx + gbeta[2] * tissue_2_idx + gbeta[3] * tissue_4_idx + gbeta[4] * gender_idx + gbeta[5] * cell_type_idx
-        #sigma_beta = pm.Exponential('sigma_beta_{0}'.format(i),1.0) ## pass the saved lambda from the logistic regression
-        sigma_beta = np.sqrt(lambdas)
-        betas = pm.Normal('beta_{0}'.format(i), mu=mu_gbeta,sigma=sigma_beta,dims="protocol")
-        beta_list.append(betas)
+        # prior stddev in intercepts & slopes (variation across protocol):
+        sd_dist = pm.Exponential.dist(1.0)
+        
+        # get back standard deviations and rho:
+        chol, corr, stds = pm.LKJCholeskyCov("chol", n=X4_bayes.shape[1], eta=2.0, sd_dist=sd_dist, compute_corr=True)
+        
+        #hyperpriors and priors for average betas:
+        beta_list = []
+        for i in range(X4_bayes.shape[1]):
+            gbeta = pm.Normal("g_beta_{0}".format(i), mu=0.0, sigma=10.0, shape=6)
 
-    #population of varying protocol effects:
-    beta_protocol = pm.MvNormal("beta_protocol", mu=pt.stack(beta_list, axis=1), chol=chol,dims=('protocol', 'params'))
-    
-    #Expected value per protocol:
-    theta = beta_protocol[protocol_idx,0]* x[:,0]
+            mu_gbeta = gbeta[0] + gbeta[1] * organism_idx + gbeta[2] * tissue_2_idx + gbeta[3] * tissue_4_idx + gbeta[4] * gender_idx + gbeta[5] * cell_type_idx
+            #sigma_beta = pm.Exponential('sigma_beta_{0}'.format(i),1.0) ## pass the saved lambda from the logistic regression
+            sigma_beta = np.sqrt(lambdas)
+            betas = pm.Normal('beta_{0}'.format(i), mu=mu_gbeta,sigma=sigma_beta,dims="protocol")
+            beta_list.append(betas)
 
-    for i in range(1,X4_bayes.shape[1]):
-        theta += beta_protocol[protocol_idx,i] * x[:,i]
+        #population of varying protocol effects:
+        beta_protocol = pm.MvNormal("beta_protocol", mu=pt.stack(beta_list, axis=1), chol=chol,dims=('protocol', 'params'))
+        
+        #Expected value per protocol:
+        theta = beta_protocol[protocol_idx,0]* x[:,0]
 
-    p = 1.0 / (1.0 + pt.exp(-theta))
-    likelihood = pm.Bernoulli('likelihood', p, observed=Y4_bayes[0], shape = p.shape)
-    
+        for i in range(1,X4_bayes.shape[1]):
+            theta += beta_protocol[protocol_idx,i] * x[:,i]
 
-print('Done model setup')
-print('Running model')
+        p = 1.0 / (1.0 + pt.exp(-theta))
+        likelihood = pm.Bernoulli('likelihood', p, observed=Y4_bayes[0], shape = p.shape)
+        
 
-# Run the model
-with assay_level_model:
-    tr_assay = pm.sample(500, tune=500,cores=4,chains=4,init="adapt_diag")
+    print('Done model setup')
+    #print('Running model')
 
-filename = 'results/tr_assay_full_model.pkl'
-pickle.dump(tr_assay, open(filename, 'wb'))
-print('model saved')
+    # Run the model
+    #with assay_level_model:
+    #    tr_assay = pm.sample(500, tune=500,cores=4,chains=4,init="adapt_diag")
 
+    #filename = 'results/tr_assay_full_model.pkl'
+    #pickle.dump(tr_assay, open(filename, 'wb'))
+    #print('model saved')
+    return 0
+
+
+if __name__=="__main__":
+    sys.exit(main())
