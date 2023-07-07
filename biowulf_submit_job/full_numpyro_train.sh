cafff5bca0bdbf98e32e9bc0c3ab00a34cb3562d
Wenyu Zeng <50471174+wzeng96@users.noreply.github.com>
2023-07-07 21:09:42 +0800

    Update full_numpyro_train.sh

--- biowulf_submit_job/full_numpyro_train.sh
+++ biowulf_submit_job/full_numpyro_train.sh
@@ -2,12 +2,12 @@
 #
 #SBATCH --mem=40g
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:p100:2
+#SBATCH --gres=gpu:v100x:4
 
 #SBATCH --cpus-per-task=14
 #SBATCH --time=168:00:00
 #SBATCH --mail-type=END
-#SBATCH --output=R-0612.%j.out
-#SBATCH --error=R-0612.%j.err
+#SBATCH --output=R.%j.out
+#SBATCH --error=R.%j.err
 
 python Tox21/PyMC_model_train.py
